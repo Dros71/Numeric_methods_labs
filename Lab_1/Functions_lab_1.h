65209c331e4bd2893c2a1e257fbdc6485a4cc214
Suchazzme <suchazzme@gmail.com>
2023-11-16 20:35:51 +0300

    ignore usless files

--- Lab_1/Functions_lab_1.h
+++
@@ -1,9 +0,0 @@
-#include"Gauss_Function.h"
-#include <iostream>
-#include <cmath>
-#include <fstream>
-#include <clocale>
-using namespace std;
-
-void Input(ifstream&, double**, double**, double*, double*, int);
-void FreeMemory(double**, double**, double*, double*, double*, double*, double*, double*, int);
--- Lab_2/Gauss_Function.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-void OutputMatrix(double**, double*, int);
-void CheckBefore(double**, double*, int, int);
-void DoTheXnOne(double**, double*, int, int);
-void Gauss(double**, double*, int);
-void FindTheAnswer(double**, double*, double*, int);
-
