8fbe0032644f61e0e90177f22c3487287235ad91
Suchazzme <suchazzme@gmail.com>
2023-11-20 23:57:23 +0300

    1/3 lab done!

--- Lab_5/Functions_lab_5.cpp
+++ Lab_5/Functions_lab_5.cpp
@@ -1,20 +1,20 @@
 #include "Functions_lab_5.h"
 
 double function_value(double x) {
-	return (1 + x + x * x) / pow((x * x * x - 1), 1 / 2);
+	return (1 + x + x * x) / sqrt(x * x * x - 1);		// x != 0 !!!
 }
 
 double trapezoid_method() {
 	double h = (b - a) / n;
-	double integral_trapezoid_h = (function_value(a) + function_value(b) * h) / 2;
-	double integral_trapezoid_h2 = (function_value(a) + function_value(b) * h / 2) / 2;
-	double x = a;
-	while (integral_trapezoid_h2 - integral_trapezoid_h > (3 * eps1)){
+	double integral_trapezoid_h = ((function_value(a) + function_value(b)) * h) / 2;
+	double integral_trapezoid_h2 = ((function_value(a) + function_value(b)) * h / 2) / 2;
+	while (fabs(integral_trapezoid_h2 - integral_trapezoid_h) >= (3 * eps1)){
+		
 		integral_trapezoid_h = integral_trapezoid_h2;
 		h /= 2;
-		for (double x = a; x <= b; x += h / 2)
-		integral_trapezoid_h2 = h * function_value(x);
-		cout << "Hello" << '\n';
+		integral_trapezoid_h2 = ((function_value(a) + function_value(b)) * h) / 2;
+		for (double x = a + h; x < b; x += h)
+		integral_trapezoid_h2 += h * function_value(x);
 	}
 	return integral_trapezoid_h2;
 }
@@ -22,16 +22,24 @@ double trapezoid_method() {
 double Simpsons_method() {
 	double h = (b - a) / (2 * n);
 	int m = n / 2;
-	double integral_Simpsons = ((a + b) * h) / 3;
-	double x = a;
-	for (int i = 0; i < m; i++) {
-		x += 2 * h;
-		integral_Simpsons += (h / 3) * 4 * function_value(x);
+	double integral_Simpsons_h = (function_value(a) + function_value(b)) * h / 3;
+	double integral_Simpsons_h2 = (function_value(a) + function_value(b)) * h / 2 / 3;
+	
+	while (fabs(integral_Simpsons_h2 - integral_Simpsons_h) >= (15 * eps1)) {
+		integral_Simpsons_h = integral_Simpsons_h2;
+		h /= 2;
+		m *= 2;
+		integral_Simpsons_h2 = (function_value(a) + function_value(b)) * h / 3;
+		double x = a + h;
+		for (int i = 1; i <= m; i++) {
+			integral_Simpsons_h2 += 4 * h / 3 * function_value(x);
+			x += 2 * h;
+		}
+		x = a + 2 * h;
+		for (int i = 2; i < m; i++) {
+			integral_Simpsons_h2 += 2 * h / 3 * function_value(x);
+			x += 2 * h;
+		}
 	}
-	x = a;
-	for (int i = 0; i < m - 1; i++) {
-		x += 2 * h;
-		integral_Simpsons += (h / 3) * 2 * function_value(x);
-	}
-	return integral_Simpsons;
+	return integral_Simpsons_h2;
 }
--- Lab_5/Functions_lab_5.h
+++ Lab_5/Functions_lab_5.h
@@ -3,9 +3,10 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
-const double a = 1.0; const double b = 2.631;
+const double a = 1.001;
+const double b = 2.631;
 const double eps1 = 1e-4; const double eps2 = 1e-5;
-const int n = 100;
+const int n = 2;
 
 
 double function_value(double);
