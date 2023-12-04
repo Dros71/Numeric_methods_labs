4b19ad0c8ff91a2dc71e25fb05874a3c81cbab96
Suchazzme <suchazzme@gmail.com>
2023-12-04 20:50:25 +0300

    Lab_4 done(sent for testing)

--- Lab_4/Lab_4.cpp
+++ Lab_4/Lab_4.cpp
@@ -3,12 +3,14 @@
 int main()
 {
     for (int i = 0; i < n; i++) 
-        cout << " x =" << x[i] << ". y = " << y[i] << endl;
+        cout << " x = " << x[i] << ". y = " << y[i] << endl;
     cout << endl;
     double* A = new double[m + 1];
     least_square_method(A);
-    cout << "a = " << A[0] << endl;
-    cout << "b = " << A[1] << endl;
+    cout << "a = " << A[1] << endl;
+    cout << "b = " << A[0] << endl;
+    for (int i = 0; i < n; i++)
+        cout << "Expectation: " << (A[1] / x[i]) + A[0] << " Reality: " << y[i] << endl;
     double eps = Count_eps(A);
     cout << "eps = " << eps << endl;
     cout << "Answer: " << "y = " << A[1] << "/x + " << A[0];
